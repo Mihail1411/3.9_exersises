65971aa082960eabd4b359aa151cefa02ae2b9af
d.yacenko <d.yacenko@gmail.com>
2018-04-15 19:09:03 +0300

    Setup model

--- SimpleGame.3.9.1/core/src/ru/samsung/itschool/SimpleGame.java
+++ SimpleGame.3.9.1/core/src/ru/samsung/itschool/SimpleGame.java
@@ -10,31 +10,50 @@ import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
 
 public class SimpleGame extends ApplicationAdapter {
-	public PerspectiveCamera cam;
-	final float[] startPos = {150f, -9f, 0f};
-	public Model model;
-	public ModelInstance instance;
+    public PerspectiveCamera cam;
+    final float[] startPos = {150f, -9f, 0f};
+    public Model model;
+    public ModelInstance instance;
+    public ModelBatch modelBatch;
 
 
-	@Override
-	public void create() {
-		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(startPos[0], startPos[1], startPos[2]);
-		cam.lookAt(0, 0, 0);
-		cam.near = 1f;
-		cam.far = 300f;
-		cam.update();
-		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createCone(20f, 120f, 20f, 3,
-				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-		instance = new ModelInstance(model);
-		instance.transform.setToRotation(Vector3.Z, 120);
-	}
+    @Override
+    public void create() {
+        modelBatch = new ModelBatch();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(startPos[0], startPos[1], startPos[2]);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createCone(20f, 120f, 20f, 3,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+        instance.transform.setToRotation(Vector3.Z, 120);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance);
+        modelBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        model.dispose();
+        modelBatch.dispose();
+    }
 }
